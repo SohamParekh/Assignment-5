d261db42785163dcf403ac48357b7d4719400bd3
Soham <soham.parekh100639@marwadiuniversity.ac.in>
2020-01-28 18:17:17 +0530

    First Commit

---
+++ Assignment-5/PM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    public class PM
+    {
+        public PM(string pm_name,int pm_year)
+        {
+            this.pm_name = pm_name;
+            this.pm_year = pm_year;
+        }
+        public string pm_name { get; set; }
+        public int pm_year { get; set; }
+        public override string ToString()
+        {
+            return "PrimeMinister Name:-" + pm_name + " " + "Year:-" + pm_year;
+        }
+    }
+}
---
+++ Assignment-5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, PM> dict = new Dictionary<string, PM>();
+            dict.Add("First", new PM("Atal Bihari Vajpayee", 1998));
+            dict.Add("second", new PM("Narendra Modi", 2014));
+            dict.Add("third", new PM("Manmohan Singh", 2004));
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nAdded Current PM of 2019 Election :-");
+            dict["forth"] = new PM("Narendra Modi", 2019);
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nSorted By year :-");
+            var sortyear = dict.OrderBy(d => d.Value.pm_year);
+            foreach (var primeMini in sortyear)
+                Console.WriteLine(primeMini);
+            Console.Read();
+        }
+    }
+}
